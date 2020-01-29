d41768b5863c938fb346bf0c403c52858bbac1c4
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-01-29 10:51:37 -0300

    CM - Adicionado IocServices para DI do Service

---
+++ TaskList.Application/Ioc/IocService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TaskList.Application.Services;
+using TaskList.Application.Services.Interfaces;
+
+namespace TaskList.Application.Ioc
+{
+    public static class IocService
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddScoped<ITaskService, TaskService>();
+        }
+    }
+}
