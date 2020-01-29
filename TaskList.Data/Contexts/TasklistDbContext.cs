4735a9b0bfd01854548fc5328890a0256ac35f05
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-01-29 09:22:17 -0300

    CM - Adicionado Contexto do banco de dados

---
+++ TaskList.Data/Contexts/TasklistDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskList.Domain;
+
+namespace TaskList.Data.Contexts
+{
+    public class TasklistDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+
+        public TasklistDbContext(DbContextOptions<TasklistDbContext> options) : base(options)
+        {
+        }
+    }
+}
