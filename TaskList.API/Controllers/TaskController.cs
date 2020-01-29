053dcc8d0d49074cf47eaf5eb2694d858560dbfc
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-01-29 10:54:48 -0300

    CM - Implementado TaskController

---
+++ TaskList.API/Controllers/TaskController.cs
@@ -0,0 +1,104 @@
+﻿using DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TaskList.Application.Services.Interfaces;
+
+namespace TaskList.API.Controllers
+{
+    [Route("api/tasks")]
+    public class TaskController : Controller
+    {
+        private readonly ITaskService _taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var tasks = await _taskService.GetAllAsync(cancellationToken);
+                return Ok(tasks);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.StackTrace);
+            }
+        }
+
+        [HttpGet("{taskId}")]
+        public async Task<IActionResult> GetById(Guid taskId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var task = await _taskService.GetAsync(taskId, cancellationToken);
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.StackTrace);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] TaskDto taskDto, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var task = await _taskService.AddAsync(taskDto, cancellationToken);
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.StackTrace);
+            }
+        }
+
+        [HttpPut()]
+        public async Task<IActionResult> Update([FromBody] TaskDto taskDto, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var task = await _taskService.UpdateAsync(taskDto, cancellationToken);
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.StackTrace);
+            }
+        }
+
+        [HttpDelete("{taskId}")]
+        public async Task<IActionResult> Delete(Guid taskId, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var task = await _taskService.DeleteAsync(taskId, cancellationToken);
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.StackTrace);
+            }
+        }
+
+        [HttpPut("{taskId}/{status}")]
+        public async Task<IActionResult> UpdateStatus(Guid taskId, bool status, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var task = await _taskService.UpdateStatusAsync(taskId, status, cancellationToken);
+                return Ok(task);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.StackTrace);
+            }
+        }
+    }
+}
