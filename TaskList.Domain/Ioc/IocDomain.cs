2e79022071fbabc1acd16ec1a78c812db9e9de18
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2020-01-29 10:49:49 -0300

    CM - Adicionado IocDomain para DI do builder

---
+++ TaskList.Domain/Ioc/IocDomain.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TaskList.Domain.Builders;
+
+namespace TaskList.Domain.Ioc
+{
+    public static class IocDomain
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddScoped<ITaskBuilder, TaskBuilder>();
+        }
+    }
+}
